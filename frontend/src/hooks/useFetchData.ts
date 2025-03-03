3f3657bbddff498b0c21f0a96ff540109e36e622
Harry <harrycarr64@hotmail.com>
2025-03-03 22:09:17 +0000

    Update axios base URL to use environment variable for better configuration

--- frontend/src/hooks/useFetchData.ts
+++ frontend/src/hooks/useFetchData.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse, isAxiosError } from 'axios'
 
 const useFetchData = async <T>(path: string): Promise<T> => {
-    axios.defaults.baseURL = 'http://localhost:5000/api'
+    axios.defaults.baseURL = process.env.BACKEND_API_URL || 'http://localhost:5000/api'
     return axios
         .get<T>(path)
         .then((response: AxiosResponse<T>) => {
