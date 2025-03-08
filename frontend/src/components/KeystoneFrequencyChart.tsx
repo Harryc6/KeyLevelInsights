f33c5bdae76c0b75e4a176b4655999299bf241df
Harry <harrycarr64@hotmail.com>
2025-03-08 03:30:39 +0000

    Remove commented example data and enable bar value labels in KeystoneFrequencyChart

--- frontend/src/components/KeystoneFrequencyChart.tsx
+++ frontend/src/components/KeystoneFrequencyChart.tsx
@@ -6,20 +6,6 @@ import { ErrorBoundary } from 'react-error-boundary'
 const KeystoneFrequencyChart: FC<{ period: number; dungeon?: number }> = ({ period, dungeon }) => {
     const { data } = useGetKeystoneFrequency(period, dungeon)
 
-    // const exampleData = [
-    //     { keystoneLevel: 2, runs: '90861' },
-    //     { keystoneLevel: 3, runs: '31660' },
-    //     { keystoneLevel: 4, runs: '40935' },
-    //     { keystoneLevel: 5, runs: '30353' },
-    //     { keystoneLevel: 6, runs: '54723' },
-    //     { keystoneLevel: 7, runs: '50881' },
-    //     { keystoneLevel: 8, runs: '40940' },
-    //     { keystoneLevel: 9, runs: '17253' },
-    //     { keystoneLevel: 10, runs: '12596' },
-    //     { keystoneLevel: 11, runs: '576' },
-    //     { keystoneLevel: 12, runs: '23' },
-    // ]
-
     return (
         <ErrorBoundary fallback={<div>Failed to load keystone frequency data</div>}>
             <BarChart
@@ -30,6 +16,7 @@ const KeystoneFrequencyChart: FC<{ period: number; dungeon?: number }> = ({ peri
                 series={[{ name: 'runs', label: 'Total Runs', color: 'violet' }]}
                 xAxisLabel={'Keystone Level'}
                 withTooltip={false}
+                withBarValueLabel={true}
             />
         </ErrorBoundary>
     )
